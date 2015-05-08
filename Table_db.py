3fd0b7790c117ddd0cd5e4387a867741cd5856cb
deyanskiq <deyanskiq@abv.bg>
2015-05-08 15:28:16 +0300

    fixing mistakes

--- Table_db.py
+++ Table_db.py
@@ -4,7 +4,8 @@ from settings import DB_NAME, SQL_FILE
 
 
 conn = sqlite3.connect(DB_NAME)
+cursor = conn.cursor()
 
 with open(SQL_FILE, "r") as f:
-    conn.executescript(f.read())
-    conn.commit()
+    cursor.executescript(f.read())
+cursor.commit()
