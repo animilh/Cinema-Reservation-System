bb3b3bdf8e50e426d5fdd89dd76bf0b07d9cb2dc
Ana Burnaska <ahristova.milenova@gmail.com>
2015-05-09 18:11:50 +0300

    added functions for spell commands, start() and main()

--- magic_reservation_system.py
+++ magic_reservation_system.py
@@ -14,46 +14,85 @@ spells = [
     'help'
 ]
 
-print('Welcome to Magic Cinema Reservation System')
+def help_me():
+    print('You have the following options:')
+    for spell in spells:
+        print(spell)
 
-while True:
-    choice = input('>')
-    if str(choice) not in spells:
-        print('Bad input. For more information please type help')
-        continue
+def show_movies():
+    movies = Movie.show_movies(connection)
+    print("Current movies:")
+    for movie in movies:
+        print('[{}] - {} ({})'.format(movie[0], movie[1], movie[2]))
 
-    if str(choice) == 'help':
-        print('You have the following options:')
-        for spell in spells:
-            print(spell)
-        continue
+def get_movie(id_movie):
+    movie_name = Movie.get_movie(connection, id_movie)
+    print("Projections for movie '{}':".format(movie_name['name']))
 
-    if choice == 'show_movies':
-        movies = Movie.show_movies(connection)
-        print("Current movies:")
-        for movie in movies:
-            print('[{}] - {} ({})'.format(movie[0], movie[1], movie[2]))
-        continue
+def get_movie_projections(id_movie):
+    projections = Movie.get_movie_projections(connection, id_movie)
+    for proj in projections:
+        print('[{}] - {} {} ({})'.format(proj[0], proj[1], proj[2], proj[3]))
 
-    if choice == 'show_movie_projections':
-        id_movie = input('id movie>')
-        movie_name = Movie.get_movie(connection, id_movie)
-        print("Projections for movie '{}':".format(movie_name['name']))
-        projections = Movie.get_movie_projections(connection, id_movie)
-        for proj in projections:
-            print('[{}] - {} {} ({})'.format(proj[0], proj[1], proj[2], proj[3]))
-        continue
+def cancel_reservation(name):
+    Movie.cancel_reservation(connection, name)
+    #unmark the seats from the kinosalon
+    print("The reservation of {} has been canceled.".format(name))
 
-    if str(choice) == 'exit':
-        print('Exit the Magic System')
-        break
+def start():
+    print('Welcome to Magic Cinema Reservation System')
 
-    if str(choice) == 'cancel_reservation':
-        name = input('name>')
-        Movie.cancel_reservation(connection, name)
-        #unmark the seats from the kinosalon
-        print("The reservation of {} has been canceled.".format(name))
+    while True:
+        choice = input('>')
+        if str(choice) not in spells:
+            print('Bad input. For more information please type help')
+            continue
 
-    if str(choice) == 'make_reservation':
-        username = input("Step 1 (User): Choose name>")
-        number_of_tickets = input("Step 1 (User): Choose number of tickets>")
+        if str(choice) == 'help':
+            help_me()
+            continue
+
+        if choice == 'show_movies':
+            show_movies()
+            continue
+
+        if choice == 'show_movie_projections':
+            id_movie = input('id movie>')
+            get_movie(id_movie)
+            get_movie_projections(id_movie)
+            continue
+
+        if str(choice) == 'exit':
+            print('Exit the Magic System')
+            break
+
+        if str(choice) == 'cancel_reservation':
+            name = input('name>')
+            cancel_reservation(name)
+            continue
+
+        if str(choice) == 'make_reservation':
+            username = input("Step 1 (User): Choose name>")
+            number_of_tickets = input("Step 1 (User): Choose number of tickets>")
+            show_movies()
+            id_movie = input("Step 2 (Movie): Choose a movie>")
+            get_movie(id_movie)
+            get_movie_projections(id_movie)
+            id_projection = input("Step 3 (Projection): Choose a projection>")
+            #print a map of cinema
+            #Step 4 (Seats): Choose seat 1>
+            #Step 4 (Seats): Choose seat 2>
+            #print the reservation
+            #insert the reservation in table reservations!!!
+            final = input("Step 5 (Confirm - type 'finalize')>")
+            if final == 'finalize':
+                print('Thanks!')
+            else:
+                print('Type help to see other options.')
+            continue
+
+def main():
+    start()
+
+if __name__ == '__main__':
+    main()
