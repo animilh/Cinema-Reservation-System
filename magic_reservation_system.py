1311135acec8de714546dc535a45a176627129b9
Ana Burnaska <ahristova.milenova@gmail.com>
2015-05-09 15:50:10 +0300

    added exit and help spells

--- magic_reservation_system.py
+++ magic_reservation_system.py
@@ -1,7 +1,8 @@
 import sqlite3
 from movie import Movie
+from settings import DB_NAME
 
-connection = sqlite3.connect('cinema.db')
+connection = sqlite3.connect(DB_NAME)
 connection.row_factory = sqlite3.Row
 cursor = connection.cursor()
 
@@ -22,25 +23,36 @@ while True:
     if str(choice) not in spells:
         print('Bad input. For more information please type help')
         continue
-    break
+
+    if str(choice) == 'help':
+        print('You have the following options:')
+        for spell in spells:
+            print(spell)
+        continue
+
+    if choice == 'show_movies':
+        movies = Movie.get_movies(connection)
+        print("Current movies:")
+        for movie in movies:
+            print('[{}] - {} ({})'.format(movie[0], movie[1], movie[2]))
+        continue
+
+    if choice == 'show_movie_projections':
+        id_movie = input('id movie>')
+        projections = Movie.get_movie_projections(connection, id_movie)
+        for proj in projections:
+            print('[{}] - {} {} ({})'.format(proj[0], proj[1], proj[2], proj[3]))
+        continue
+
+    if str(choice) == 'exit':
+        print('Exit the Magic System')
+        break
+
+    if str(choice) == 'cancel_reservation':
+        name = input('name>')
 
 
-if choice == 'help':
-    print('You have the following options:')
-    for spell in spells:
-        print(spell)
 
-if choice == 'show_movies':
-    movies = Movie.get_movies(connection)
-    print("Current movies:")
-    for movie in movies:
-        print('[{}] - {} ({})'.format(movie[0], movie[1], movie[2]))
-
-if choice == 'show_movie_projections':
-    id_movie = input('id movie>')
-    projections = Movie.get_movie_projections(connection, id_movie)
-    for proj in projections:
-        print('[{}] - {} {} ({})'.format(proj[0], proj[1], proj[2], proj[3]))
 
 # if choice == 'make_reservation':
 #     Movie.make_reservation()
@@ -48,5 +60,4 @@ if choice == 'show_movie_projections':
 # if choice == 'cancel_reservation':
 #     Movie.cancel_reservation()
 
-if choice == 'exit':
-    print('Press ^D to exit the Magic System')
+
