ad0e9e847144b2b06e3438805901c6416ece5722
Ana Burnaska <ahristova.milenova@gmail.com>
2015-05-09 17:31:50 +0300

    added functions cancel_reservation and get_movie

--- movie.py
+++ movie.py
@@ -1,9 +1,15 @@
 class Movie:
 
-    SHOW_MOVIE = """
-        SELECT id, name, raiting
+    SHOW_MOVIES = """
+        SELECT id, name, rating
         FROM movies
         ORDER BY id
+    """
+
+    GET_MOVIE = """
+        SELECT name
+        FROM movies
+        WHERE id = ?
     """
 
     SHOW_MOVIE_PROJECTIONS = """
@@ -12,15 +18,33 @@ class Movie:
         WHERE movie_id = ?
     """
 
+    CANCEL_RESERVATION = """
+        DELETE FROM reservations
+        WHERE username = ?
+    """
+
 
     @classmethod
-    def get_movies(cls, connection):
+    def show_movies(cls, connection):
         cursor = connection.cursor()
-        result = cursor.execute(cls.SHOW_MOVIE)
+        result = cursor.execute(cls.SHOW_MOVIES)
         return result.fetchall()
+
+    @classmethod
+    def get_movie(cls, connection, movie_id):
+        cursor = connection.cursor()
+        movie_name = cursor.execute(cls.GET_MOVIE, (movie_id, ))
+        return movie_name.fetchone()
 
     @classmethod
     def get_movie_projections(cls, connection, movie_id):
         cursor = connection.cursor()
         result = cursor.execute(cls.SHOW_MOVIE_PROJECTIONS, (movie_id, ))
         return result.fetchall()
+
+    @classmethod
+    def cancel_reservation(cls, connection, name):
+        cursor = connection.cursor()
+        cursor.execute(cls.CANCEL_RESERVATION, (name, ))
+        connection.commit()
+
