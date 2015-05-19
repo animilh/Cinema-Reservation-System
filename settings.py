1dfdd209b19bbf57096a90a946c9b87afb1e4845
Ana Burnaska <ahristova.milenova@gmail.com>
2015-05-19 12:13:34 +0300

    added new settings

--- settings.py
+++ settings.py
@@ -1,2 +1,15 @@
+from kinosalon import Map
+
 DB_NAME = "cinema.db"
 SQL_FILE = "Tables.sql"
+
+spells = [
+    'show_movies',
+    'show_movie_projections',
+    'make_reservation',
+    'cancel_reservation',
+    'exit',
+    'help'
+]
+
+cn = Map("kinosalon.txt")
